099eb17f4514e1f72d12d9f3ec3b0b10cda5dfbb
Andrea Botello <andrea.botellor@misena.edu.co>
2025-11-11 11:55:29 -0500

    Ajustes CFA [1.3]

--- src/config/global.js
+++ src/config/global.js
@@ -174,7 +174,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_22650000_DU.pdf',
       },
       {
         icono: 'fas fa-download',
